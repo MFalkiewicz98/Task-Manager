58292a273e72477696bd0f1420c3d4ae13f31690
MFalkiewicz98 <maks.falkiewicz@gmail.com>
2021-10-29 17:31:37 +0200

    first commit

---
+++ Repositories/TaskManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task_Manager.Models;
+
+namespace Task_Manager.Repositories
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+
+    }
+}
